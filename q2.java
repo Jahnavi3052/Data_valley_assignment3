d42f618fb350f78ac41a86ad3318603f6e3038b8
Bammidi Jahnavi <111736896+Jahnavi3052@users.noreply.github.com>
2024-03-18 16:31:01 +0530

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,58 @@
+class Counter {
+    private int cnt = 0;
+    public synchronized void increment() {
+        cnt++;
+    }
+    public synchronized void decrement() {
+        cnt--;
+    }
+    public synchronized int getCount() {
+        return cnt;
+    }
+}
+
+class IncrementThread extends Thread {
+    private Counter counter;
+
+    public IncrementThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+}
+
+class DecrementThread extends Thread {
+    private Counter counter;
+
+    public DecrementThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.decrement();
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+
+        IncrementThread incrementThread = new IncrementThread(counter);
+        DecrementThread decrementThread = new DecrementThread(counter);
+
+        incrementThread.start();
+        decrementThread.start();
+
+        incrementThread.join();
+        decrementThread.join();
+
+        // Output the final count
+        System.out.println("Final count: " + counter.getCount());
+    }
+}
